f80006659e0ba8e523c826c7c7879b5be23fd58e
Daniel Niasoff <4070285+dniasoff@users.noreply.github.com>
2023-03-26 15:45:37 +0000

    added support for duplicate published_app_ids

--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = ">= 2.7.0"
+      version = ">= 2.36.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.59.0"
+      version = ">= 3.49.0"
     }
     random = {
       source  = "hashicorp/random"
