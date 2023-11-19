89aa72115c3c930772febbe3b68095c74a3019b7
Daniel Niasoff <4070285+dniasoff@users.noreply.github.com>
2023-11-19 12:13:01 +0000

    fixed deprecation messages

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 
 output "client_id" {
   description = "The application id of AzureAD application created."
-  value       = azuread_application.this.application_id
+  value       = azuread_application.this.client_id
 }
 
 output "tenant_id" {
@@ -12,11 +12,6 @@ output "tenant_id" {
 output "service_principal_object_id" {
   description = "The object id of service principal. Can be used to assign roles to user."
   value       = azuread_service_principal.this.object_id
-}
-
-output "service_principal_application_id" {
-  description = "The object id of service principal. Can be used to assign roles to user."
-  value       = azuread_service_principal.this.application_id
 }
 
 output "application_object_id" {
